0537e70ec7f5e54bfc1b5b4d02dae442c0222f07
EssenceOfChaos <fjschiller@gmail.com>
2018-08-02 03:21:40 -0400

    change to env.prod for correct uri

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Employee } from './employee';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
